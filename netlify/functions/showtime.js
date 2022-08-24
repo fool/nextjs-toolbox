a5741eccb90d59de6c83ddcce1c0b198122e40df
fool <chrism@netlify.com>
2022-08-24 11:59:08 -0700

    trying a function

---
+++ netlify/functions/showtime.js
@@ -0,0 +1,17 @@
+const { builder } = require("@netlify/functions")
+
+async function handler(event, context) {
+
+
+  var todayDate = new Date().toISOString();
+  return {
+    statusCode: 200,
+    headers: {
+      "Content-Type": "text/html",
+    },
+    body: `<html>${todayDate}</html>`
+  };
+console.log(todayDate)
+}
+
+exports.handler = builder(handler);
